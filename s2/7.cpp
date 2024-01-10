283b9de92e047186a5ac58a2f94bb31d0672927c
hiradsajde <hi@hiradsajde.ir>
2024-01-10 19:45:04 +0330

    season 2 question 7 added

---
+++ s2/7.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std; 
+
+int main(){
+    int a; 
+    cin >> a; 
+    int apow2 = a * a; 
+    int apow3 = a * a * a; 
+    cout << apow2 << endl << apow3; 
+}
