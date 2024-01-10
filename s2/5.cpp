b0b103531638d60006e9b07659b218d83e8542d9
hiradsajde <hi@hiradsajde.ir>
2024-01-10 19:37:21 +0330

    season 2 question 5 added

---
+++ s2/5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std; 
+
+int main(){
+    float r , p=3.14 ; 
+    cin >> r; 
+    float s = p * r * r; 
+    float d = 2 * p * r; 
+    cout << s << endl << d;
+    return 0;
+}
